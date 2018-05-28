b799b4073a34a0d5992ab3113dc9ce6c21f8b048
Unknown <Florian.HUREAU@etu.uca.fr>
2018-05-28 08:36:26 +0200

    Auto stash before merge of "master" and "origin/master"

--- masterDetail/src/app/app.module.ts
+++ masterDetail/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+<<<<<<< Updated upstream
 
 import { AgmCoreModule } from '@agm/core';
 
+=======
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+>>>>>>> Stashed changes
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AgmCoreModule } from '@agm/core';  
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +25,18 @@ import { HomePage } from '../pages/home/home';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+<<<<<<< Updated upstream
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAF9w5Gw-o5L5et8ywyS_dfntBPaRRwzmA'
+=======
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_KEY'
+>>>>>>> Stashed changes
     })
   ],
-  bootstrap: [IonicApp],
+  bootstrap: [IonicApp, AppComponent],
   entryComponents: [
     MyApp,
     HomePage
