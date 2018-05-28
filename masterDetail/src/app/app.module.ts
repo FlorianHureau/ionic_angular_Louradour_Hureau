c67c2f9b2cbe16dc81f140060431cf2b24e686d0
Unknown <Florian.HUREAU@etu.uca.fr>
2018-05-28 08:55:16 +0200

    Correction des conflits

--- masterDetail/src/app/app.module.ts
+++ masterDetail/src/app/app.module.ts
@@ -3,19 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-<<<<<<< Updated upstream
-
 import { AgmCoreModule } from '@agm/core';
-
-=======
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
->>>>>>> Stashed changes
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { AgmCoreModule } from '@agm/core';  
-
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -25,18 +17,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-<<<<<<< Updated upstream
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAF9w5Gw-o5L5et8ywyS_dfntBPaRRwzmA'
-=======
-    CommonModule,
-    FormsModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'YOUR_KEY'
->>>>>>> Stashed changes
     })
   ],
-  bootstrap: [IonicApp, AppComponent],
+  bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage
