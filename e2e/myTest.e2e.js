54f58efe537ffb4f74f96e69149086423777100a
Unknown <Florian.HUREAU@etu.uca.fr>
2018-06-12 19:18:42 +0200

    last modifs

--- e2e/myTest.e2e.js
+++ e2e/myTest.e2e.js
@@ -1,11 +1,4 @@
 
 describe('Je m\'authentifie', ()=> {
-    // ...    
-/*
-    it('Mes identifiants sont mauvais', ()=> {
-        // Accès à la première page de l'application
-        browser.get('/');
-        // ... Réalisation du scénario de test
-        expect(...).toBe(...);
-    })*/
+
 })
