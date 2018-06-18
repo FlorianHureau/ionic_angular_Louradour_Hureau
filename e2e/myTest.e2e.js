bb95ae8f30f4cc01ccb052da11ffca6782470d10
Unknown <Florian.HUREAU@etu.uca.fr>
2018-06-18 10:24:58 +0200

    Premiers tests + correctifs sur les pages pour la conformité

--- e2e/myTest.e2e.js
+++ e2e/myTest.e2e.js
@@ -1,4 +1,22 @@
-
-describe('Je m\'authentifie', ()=> {
-
+describe('J\'accède à la liste des rando', function() {
+    
+    browser.get('http://localhost:8100/');
+    it('doit accéder à la liste des randos', function(){
+        document.getElementById('bouton').click();
+        expect(browser.getTitle()).toBe('liste');
+    });
+    browser.get('http://localhost:8100/#/liste');
+    it('doit entrer dans la première rando', function(){
+        document.getElementsByClassName('singleRando').item(0).click();
+        expect(browser.getTitle().toBe('detail'));
+    });
+    browser.get('http://localhost:8100/#/liste');
+    document.getElementsByClassName('singleRando').item(0).click();
+    it('vérifie que les infos de la rando ne sont pas nulles', function(){
+        var depart=document.getElementById('lieudepart');
+        var arrivee=document.getElementById('lieuarrivee');
+        var temps=document.getElementById('minutes');
+        var distance=document.getElementById('distance');
+        expect(distance.not.toBe(null) && temps.not.toBe(null) && depart.not.toBe(null) && arrivee.not.toBe(null));
+    });
 })
